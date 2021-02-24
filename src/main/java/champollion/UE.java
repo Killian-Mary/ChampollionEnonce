536df4a8ccc5c71d3c34543e577e6ece6b5057cf
Killian MARY <killian.mary@etud.univ-jfc.fr>
2021-02-24 12:29:28 +0100

    Implementation de la classe UE et ajout des fichiers untracked

--- src/main/java/champollion/UE.java
+++ src/main/java/champollion/UE.java
@@ -1,15 +1,23 @@
 package champollion;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UE {
     private final String myIntitule;
+    private ArrayList<ServicePrevu> intervenants;
 
     public UE(String intitule) {
         myIntitule = intitule;
+        this.intervenants = new ArrayList<>();
     }
 
     public String getIntitule() {
         return myIntitule;
     }
 
+    public List<ServicePrevu> getIntervenants(){
+    	return this.intervenants;
+    }
     
 }
